5e23aa02a633cac27c2603d2820882d131275f65
vitalii_belich <nsk.leopold@gmail.com>
2024-10-05 20:22:45 +0700

    delete sleep(), change html

--- src/main/java/BookListProvider.java
+++ src/main/java/BookListProvider.java
@@ -1,7 +1,7 @@
 import com.codeborne.selenide.Configuration;
-import com.codeborne.selenide.SelenideElement;
-import com.codeborne.selenide.WebDriverRunner;
+import org.openqa.selenium.By;
 
+import java.time.Duration;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -9,9 +9,9 @@ import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Condition.not;
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.*;
-import static com.codeborne.selenide.Selenide.closeWebDriver;
 
 public class BookListProvider {
 
@@ -41,6 +41,10 @@ public class BookListProvider {
                     authorName = "Ивлин Во";
                 } else if (authorName.contains("Голсуорси")) {
                     authorName = "Джон Голсуорси";
+                } else if (authorName.equals("Джейн Остен")) {
+                    authorName = "Джейн Остин";
+                } else if (authorName.contains("Стивенсон")) {
+                    authorName = "Роберт Льюис Стивенсон";
                 }
 
                 Author author = authorMap.get(authorName);
@@ -61,24 +65,29 @@ public class BookListProvider {
 
     private List<String> getBookData() {
         List<String> books = new ArrayList<>();
-        Configuration.browser = "chrome";
+        Configuration.browser = "edge";
         Configuration.pageLoadStrategy = "none";
         open("https://studyenglishwords.com/browse");
-        WebDriverRunner.getWebDriver().manage().window().maximize();
-        sleep(4000);
-        $("#inner_header > ul > li.option_type > div > a:nth-child(3)").shouldBe(visible).click(); // срабатывает не всегда
-        sleep(800);
-        $("#inner_header > ul > li.option_type > div > a:nth-child(3)").click();
-        sleep(3000); // ждать пока изменится селектор и значение
-        SelenideElement element = $("#searchResultsTitle");
-        int totalBooks = Integer.parseInt(element.getText().replaceAll("\\D+", ""));
+
+        String previousCount = $("#searchResultsTitle").getText();
+        $(By.xpath("//*[@id=\"inner_header\"]/ul/li[1]/div/a[2]")).click();
+        $("#searchResultsTitle").shouldHave(not(text(previousCount)), Duration.ofSeconds(5));
+        int totalBooks = Integer.parseInt($("#searchResultsTitle").getText().replaceAll("\\D+", ""));
         int indexBook = 1;
+        String tempBook = "temp";
 
         here:
         while (true) {
+            $("#inner_header > div.searchResults > ol > li:nth-child(1) > a").shouldHave(not(text(tempBook)), Duration.ofSeconds(5));
+
             for (int olValue = 1; olValue <= 25; olValue++) {
-                SelenideElement bookAndAuthor = $("#inner_header > div.searchResults > ol > li:nth-child(" + olValue + ") > a");
-                books.add(bookAndAuthor.getText());
+                String bookAndAuthor = $(By.xpath("//*[@id=\"inner_header\"]/div[3]/ol/li[" + olValue + "]/a")).getText();
+
+                if (olValue == 1) {
+                    tempBook = bookAndAuthor;
+                }
+
+                books.add(bookAndAuthor);
 
                 if (totalBooks == indexBook++) {
                     break here;
@@ -86,7 +95,6 @@ public class BookListProvider {
             }
 
             $("#inner_header > div.searchResults > p.paginator.paginatorNormalMode > span.next > a").click();
-            sleep(3000);
         }
 
         closeWebDriver();
--- src/main/java/GenerateHtml.java
+++ src/main/java/GenerateHtml.java
@@ -4,18 +4,19 @@ public class GenerateHtml {
 
     public String generateBooksHtml(List<Author> authors) {
         StringBuilder html = new StringBuilder();
+        int countAuthor = 1;
 
         for (Author author : authors) {
             html.append("<div class=\"author\" onclick=\"toggleBooks('").append(author.name).append("')\">\n")
-                    .append("\t").append(author.name).append("\n</div>\n")
-                    .append("<div id=\"").append(author.name).append("\" class=\"books\">\n")
-                    .append("\t<ul>\n");
+                    .append("\t&nbsp;").append(countAuthor++).append(". &nbsp;").append(author.name).append("\n")
+                    .append("\t<div id=\"").append(author.name).append("\" class=\"books\">\n")
+                    .append("\t\t<ul>\n");
 
             for (String book : author.getBooks()) {
-                html.append("\t\t<li>").append(book).append("</li>\n");
+                html.append("\t\t\t<li>").append(book).append("</li>\n");
             }
 
-            html.append("\t</ul>\n</div>\n\n");
+            html.append("\t\t</ul>\n\t</div>\n</div>\n\n");
         }
 
         return html.toString();
